d89cde3bd7ecbd4f6b396b3391419064df69b2e0
DevRLarre <dev-larre@outlook.com>
2026-03-26 23:50:08 -0300

    feat: create EmployeeUpdateDto for employee API responses, and create AdressResponse DTO for adress API response

--- src/main/java/com/example/employee_manager_api/dto/AddressResponseDto.java
+++ src/main/java/com/example/employee_manager_api/dto/AddressResponseDto.java
@@ -1,4 +1,10 @@
 package com.example.employee_manager_api.dto;
 
-public record AddressResponseDto (){
-}
+public record AddressResponseDto (
+        String street,
+        String number,
+        String neighborhood,
+        String city,
+        String state,
+        String zipCode
+){}
--- src/main/java/com/example/employee_manager_api/dto/EmployeeUpdateDto.java
+++ src/main/java/com/example/employee_manager_api/dto/EmployeeUpdateDto.java
@@ -1,4 +1,28 @@
 package com.example.employee_manager_api.dto;
 
-public record EmployeeUpdateDto {
-}
+import com.example.employee_manager_api.enums.EmployeeStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import org.hibernate.internal.build.AllowNonPortable;
+
+import java.math.BigDecimal;
+
+public record EmployeeUpdateDto (
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 150)
+        String name,
+
+        @NotBlank(message = "Job position is required")
+        @Size(max = 150)
+        String position,
+
+        @NotNull(message = "Wage is required")
+        @Positive(message = "Wage must be positive")
+        BigDecimal salary,
+
+        @NotNull(message = "Status is required")
+        EmployeeStatus status
+) {}
