5b8afd97422e7837141c93be2881751af12677ec
DevRLarre <dev-larre@outlook.com>
2026-03-26 23:35:18 -0300

    feat: create EmployeeResponseDto for employee API responses

--- src/main/java/com/example/employee_manager_api/dto/EmployeeResponseDto.java
+++ src/main/java/com/example/employee_manager_api/dto/EmployeeResponseDto.java
@@ -1,4 +1,19 @@
 package com.example.employee_manager_api.dto;
 
-public record EmployeeResponseDto () {
-}
+import com.example.employee_manager_api.entity.Adress;
+import com.example.employee_manager_api.enums.EmployeeStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record EmployeeResponseDto (
+        UUID id,
+        String name,
+        String cpf,
+        String position,
+        BigDecimal salary,
+        EmployeeStatus status,
+        LocalDate admissionDate,
+        Adress adress
+) {}
