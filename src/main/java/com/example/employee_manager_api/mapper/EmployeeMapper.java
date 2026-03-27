039a7c31b185be4623f7ff17b3d3a899ee15ee0c
DevRLarre <dev-larre@outlook.com>
2026-03-27 00:11:05 -0300

    feat: create EmployeeMapper for employee API responses,

---
+++ src/main/java/com/example/employee_manager_api/mapper/EmployeeMapper.java
@@ -0,0 +1,60 @@
+package com.example.employee_manager_api.mapper;
+
+import com.example.employee_manager_api.dto.AddressCreateDto;
+import com.example.employee_manager_api.dto.AddressResponseDto;
+import com.example.employee_manager_api.dto.EmployeeCreateDto;
+import com.example.employee_manager_api.dto.EmployeeResponseDto;
+import com.example.employee_manager_api.entity.Address;
+import com.example.employee_manager_api.entity.Employee;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeMapper {
+
+    public Employee toEntity(EmployeeCreateDto dto) {
+        return Employee.builder()
+                .name(dto.name())
+                .cpf(dto.cpf())
+                .position(dto.position())
+                .salary(dto.salary())
+                .status(dto.status())
+                .admissionDate(dto.admissionDate())
+                .address(toAddressEntity(dto.address()))
+                .build();
+    }
+
+    public EmployeeResponseDto toDto(Employee entity) {
+        return new EmployeeResponseDto(
+                entity.getId(),
+                entity.getName(),
+                entity.getCpf(),
+                entity.getPosition(),
+                entity.getSalary(),
+                entity.getStatus(),
+                entity.getAdmissionDate(),
+                toAddressDto(entity.getAddress())
+        );
+    }
+
+    private Address toAddressEntity(AddressCreateDto dto) {
+        return Address.builder()
+                .address(dto.address())
+                .number(dto.number())
+                .neighborhood(dto.neighborhood())
+                .city(dto.city())
+                .state(dto.state())
+                .zipCode(dto.zipCode())
+                .build();
+    }
+
+    private AddressResponseDto toAddressDto(Address entity) {
+        return new AddressResponseDto(
+                entity.getAddress(),
+                entity.getNumber(),
+                entity.getNeighborhood(),
+                entity.getCity(),
+                entity.getState(),
+                entity.getZipCode()
+        );
+    }
+}
